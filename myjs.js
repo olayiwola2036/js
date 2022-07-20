27ccb83956c74504834c32599ccac35b076935b9
unknown <olayiwola2036@gmail.com>
2022-07-20 04:50:43 -0700

    js

---
+++ myjs.js
@@ -0,0 +1,30 @@
+// alert('hello world')
+ 
+// let firstname='elli'
+// alert(firstname)
+
+// let human=[gender:'male',age:10,name:'teslim']
+// console.log(human)
+// let human={12,true,nul,undefined}
+// console.log(2)
+
+// let a=5
+// let b=10
+//  let c=a+b
+// console.log(c)
+
+// let a=5
+// let b=10
+// console.log(a)
+// function greet(){
+// console.log('good')
+// }
+{/* <input type="text" id="box">
+<h1 id="show"></h1>
+<button onclick="greet()">click here</button>
+function greet(){
+   let val=document.getElementById('box').Value
+   document.getElementById('show').innerHTML=val
+   console.log(val)
+} */}
+   
